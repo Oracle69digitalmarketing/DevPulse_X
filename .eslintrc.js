6ec678f51b4679ef7f771875ba20ade2a6efd7b7
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-10-04 08:23:03 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,21 @@ module.exports = {
     sourceType: 'module',
     ecmaFeatures: { jsx: true },
   },
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  settings: {
-    react: { version: 'detect' },
-  },
+  settings: { react: { version: 'detect' } },
+  env: { browser: true, es2020: true, node: true },
   plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier' // ONLY eslint-config-prettier, remove plugin:prettier/recommended
+    'plugin:prettier/recommended'
   ],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
-  },
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  }
 };
